e93270beffe3491c09dc6d0bef1a0a7840444756
Saramya68 <lakshmisaramya@gmail.com>
2026-06-25 21:18:13 +0530

    building components done

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -5,6 +5,16 @@ import arrow_icon from './arrow.png'
 import search_icon from './searchicon.png'
 import avatar_icon from './avatar-01.svg'
 import profile_martin from './profile-pic-1.jpg'
+import pp2 from './profile-pic-2.jpg'
+import pp3 from './profile-pic-3.jpg'
+import pp4 from './profile-pic-4.jpg'
+import pp5 from './profile-pic-5.jpg'
+import pp6 from './profile-pic-6.jpg'
+import pp7 from './profile-pic-3.jpg'
+import gallery_icon from './g-icon.png'
+import send_button from './sb.png'
+import mi from './m-i.jpg'
+
 
 const assets = {
     logo,
@@ -13,7 +23,20 @@ const assets = {
     arrow_icon,
     search_icon,
     avatar_icon,
-    profile_martin
+    profile_martin,
+    pp2,
+    pp3,
+    pp4,
+    pp5,
+    pp6,
+    pp7,
+    gallery_icon,
+    send_button,
+    
 }
+export const imagesDummyData = [
+   mi
+];
+
 
 export default assets
